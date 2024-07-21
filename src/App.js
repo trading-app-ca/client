a550a101faba6866f05ba1b3dad1e1d9fc7142e6
lushepherd <143170142+lushepherd@users.noreply.github.com>
2024-07-21 21:35:11 +1000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+    </Router>
+  );
+}
+
+export default App;
