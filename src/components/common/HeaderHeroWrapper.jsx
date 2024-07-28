35865acf7a7ca3afcedcf0c9633ec02cc2531132
lushepherd <143170142+lushepherd@users.noreply.github.com>
2024-07-28 14:05:51 +1000

    fix: fixed overflow issue with background

--- src/components/common/HeaderHeroWrapper.jsx
+++ src/components/common/HeaderHeroWrapper.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import Header from '../layout/Header';
 import HeroSection from '../Homepage/HeroSection';
-import MainBackground from './MainBackground';
 
 const HeaderHeroWrapper = () => {
 	return (
-		<MainBackground>
-			 <div className="header-hero-wrapper">
-				<Header />
-				<HeroSection />
-			</div>
-		</MainBackground>
+		<div className="header-hero-wrapper">
+			<Header />
+			<HeroSection />
+		</div>
 	);
 };
 
--- src/components/layout/MobileDropdown.jsx
+++ src/components/layout/MobileDropdown.jsx
@@ -1,39 +1,36 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { FaTimes } from 'react-icons/fa';
-import MainBackground from '../common/MainBackground';
 
 const MobileDropdown = ({ isOpen, onClose, links, isAuth }) => {
-  return (
-    <div className={`mobile-menu ${isOpen ? 'show' : ''}`}>
-      <MainBackground>
-        <div className="dropdown-menu-card">
-          <FaTimes size={40} className="close-icon" onClick={onClose} />
-          <nav className="dropdown-menu-nav">
-            <ul className="dropdown-menu-links">
-              {links.map((link, index) => (
-                <li key={index}>
-                  {isAuth ? (
-                    <NavLink
-                      to={link.path}
-                      className={({ isActive }) => (isActive ? 'active' : '')}
-                      onClick={onClose}
-                    >
-                      {link.label}
-                    </NavLink>
-                  ) : (
-                    <Link to={link.path} onClick={onClose}>
-                      {link.label}
-                    </Link>
-                  )}
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>  
-      </MainBackground>  
-    </div>
-  );
+    return (
+        <div className={`mobile-menu ${isOpen ? 'show' : ''}`}>
+            <div className="dropdown-menu-card">
+                <FaTimes size={40} className="close-icon" onClick={onClose} />
+                <nav className="dropdown-menu-nav">
+                    <ul className="dropdown-menu-links">
+                        {links.map((link, index) => (
+                            <li key={index}>
+                                {isAuth ? (
+                                    <NavLink
+                                        to={link.path}
+                                        className={({ isActive }) => (isActive ? 'active' : '')}
+                                        onClick={onClose}
+                                    >
+                                        {link.label}
+                                    </NavLink>
+                                ) : (
+                                    <Link to={link.path} onClick={onClose}>
+                                        {link.label}
+                                    </Link>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
 };
 
 export default MobileDropdown;
