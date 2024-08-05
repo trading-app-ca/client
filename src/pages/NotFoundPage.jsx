9f5faa37b3bccb46cede3cfb7b2c1ae155479ba2
lushepherd <143170142+lushepherd@users.noreply.github.com>
2024-08-05 19:29:56 +1000

    feat: add NotFoundPage

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Card from '../components/common/Card'; 
+import '../styles/pages/NotFoundPage.scss';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+    <Card title="404 - Page Not Found">
       <p>The page you are looking for does not exist.</p>
-      <Link to="/">Go to Home</Link>
-    </div>
+      <p>Redirecting...</p>
+    </Card>
   );
 };
 
