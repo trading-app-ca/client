9b293d34be6239fde3fe7066c6930b37134da279
lushepherd <143170142+lushepherd@users.noreply.github.com>
2024-08-09 23:08:21 +1000

    feat: integrated transactions page

--- src/pages/Transactions.jsx
+++ src/pages/Transactions.jsx
@@ -1,19 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { fakeCustomerData } from '../data';
-import TransactionsHistory from '../components/TransactionHistory';
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import TransactionHistory from '../components/TransactionHistory';
+import { AuthContext } from '../contexts/AuthContext'; 
 
 const Transactions = () => {
   const [transactions, setTransactions] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const itemsPerPage = 10; 
+  const { auth } = useContext(AuthContext);
 
   useEffect(() => {
-    setTransactions(fakeCustomerData.recentTransactions);
-  }, []);
+    const fetchTransactions = async () => {
+      try {
+        const response = await axios.get('https://crypto-trader-server.onrender.com/api/transactions', {
+          headers: {
+            Authorization: `Bearer ${auth.token}`,
+          },
+        });
+        setTransactions(response.data);
+      } catch (error) {
+        setError('Error fetching transaction data.');
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTransactions();
+  }, [auth.token]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
     <div className="content-container">
-      <TransactionsHistory 
+      <TransactionHistory 
         transactions={transactions} 
         currentPage={currentPage} 
         itemsPerPage={itemsPerPage} 
